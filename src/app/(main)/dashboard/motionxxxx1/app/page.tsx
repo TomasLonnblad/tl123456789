a266f61a8a54fa860a0e90135916b1106d9e3ae7
Tomas <tomaslonnblad@gmail.com>
2026-02-24 20:33:53 +0100

    Update page.tsx

--- src/app/(main)/dashboard/motionxxxx1/app/page.tsx
+++ src/app/(main)/dashboard/motionxxxx1/app/page.tsx
@@ -1,110 +1,167 @@
-
 "use client"
 
-import { useAnimationFrame } from "motion/react"
-import { useRef } from "react"
+import { motion, Variants } from "motion/react"
 
-export default function UseAnimationFrame() {
-    const ref = useRef<HTMLDivElement>(null)
+const draw: Variants = {
+    hidden: { pathLength: 0, opacity: 0 },
+    visible: (i: number) => {
+        const delay = i * 0.5
+        return {
+            pathLength: 1,
+            opacity: 1,
+            transition: {
+                pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+                opacity: { delay, duration: 0.01 },
+            },
+        }
+    },
+}
 
-    useAnimationFrame((t) => {
-        if (!ref.current) return
-
-        const rotate = Math.sin(t / 10000) * 200
-        const y = (1 + Math.sin(t / 1000)) * -50
-        ref.current.style.transform = `translateY(${y}px) rotateX(${rotate}deg) rotateY(${rotate}deg)`
-    })
-
+export default function PathDrawing() {
     return (
-        <div className="container">
-            <div className="cube" ref={ref}>
-                <div className="side front" />
-                <div className="side left" />
-                <div className="side right" />
-                <div className="side top" />
-                <div className="side bottom" />
-                <div className="side back" />
-            </div>
-            <StyleSheet />
-        </div>
+        <motion.svg
+            width="600"
+            height="600"
+            viewBox="0 0 600 600"
+            initial="hidden"
+            animate="visible"
+            style={image}
+        >
+            <motion.circle
+                className="circle-path"
+                cx="100"
+                cy="100"
+                r="80"
+                stroke="#ff0088"
+                variants={draw}
+                custom={1}
+                style={shape}
+            />
+            <motion.line
+                x1="220"
+                y1="30"
+                x2="360"
+                y2="170"
+                stroke="#8df0cc"
+                variants={draw}
+                custom={2}
+                style={shape}
+            />
+            <motion.line
+                x1="220"
+                y1="170"
+                x2="360"
+                y2="30"
+                stroke="#8df0cc"
+                variants={draw}
+                custom={2.5}
+                style={shape}
+            />
+            <motion.rect
+                width="140"
+                height="140"
+                x="410"
+                y="30"
+                rx="20"
+                stroke="#0d63f8"
+                variants={draw}
+                custom={3}
+                style={shape}
+            />
+            <motion.circle
+                cx="100"
+                cy="300"
+                r="80"
+                stroke="#0d63f8"
+                variants={draw}
+                custom={2}
+                style={shape}
+            />
+            <motion.line
+                x1="220"
+                y1="230"
+                x2="360"
+                y2="370"
+                stroke="#ff0088"
+                custom={3}
+                variants={draw}
+                style={shape}
+            />
+            <motion.line
+                x1="220"
+                y1="370"
+                x2="360"
+                y2="230"
+                stroke="#ff0088"
+                custom={3.5}
+                variants={draw}
+                style={shape}
+            />
+            <motion.rect
+                width="140"
+                height="140"
+                x="410"
+                y="230"
+                rx="20"
+                stroke="#8df0cc"
+                custom={4}
+                variants={draw}
+                style={shape}
+            />
+            <motion.circle
+                cx="100"
+                cy="500"
+                r="80"
+                stroke="#8df0cc"
+                variants={draw}
+                custom={3}
+                style={shape}
+            />
+            <motion.line
+                x1="220"
+                y1="430"
+                x2="360"
+                y2="570"
+                stroke="#0d63f8"
+                variants={draw}
+                custom={4}
+                style={shape}
+            />
+            <motion.line
+                x1="220"
+                y1="570"
+                x2="360"
+                y2="430"
+                stroke="#0d63f8"
+                variants={draw}
+                custom={4.5}
+                style={shape}
+            />
+            <motion.rect
+                width="140"
+                height="140"
+                x="410"
+                y="430"
+                rx="20"
+                stroke="#ff0088"
+                variants={draw}
+                custom={5}
+                style={shape}
+            />
+        </motion.svg>
     )
 }
 
 /**
  * ==============   Styles   ================
  */
-function StyleSheet() {
-    return (
-        <style>{`
-            
-        .container {
-            perspective: 800px;
-            width: 200px;
-            height: 200px;
-        }
 
-        .cube {
-            width: 200px;
-            height: 200px;
-            position: relative;
-            transform-style: preserve-3d;
-        }
-
-        .side {
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            background-color: red;
-            opacity: 0.6;
-        }
-
-        .front {
-            transform: rotateY(0deg) translateZ(100px);
-            background-color: var(--hue-1-transparent);
-        }
-        .right {
-            transform: rotateY(90deg) translateZ(100px);
-            background-color: var(--hue-2-transparent);
-        }
-        .back {
-            transform: rotateY(180deg) translateZ(100px);
-            background-color: var(--hue-3-transparent);
-        }
-        .left {
-            transform: rotateY(-90deg) translateZ(100px);
-            background-color: var(--hue-4-transparent);
-        }
-        .top {
-            transform: rotateX(90deg) translateZ(100px);
-            background-color: var(--hue-5-transparent);
-        }
-        .bottom {
-            transform: rotateX(-90deg) translateZ(100px);
-            background-color: var(--hue-6-transparent);
-        }
-
-    `}</style>
-    )
+const image: React.CSSProperties = {
+    maxWidth: "80vw",
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const shape: React.CSSProperties = {
+    strokeWidth: 10,
+    strokeLinecap: "round",
+    fill: "transparent",
+}
