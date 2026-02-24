4d13db63df72249dd38683dedd922fd946d6aa23
Tomas <tomaslonnblad@gmail.com>
2026-02-24 18:24:51 +0100

    Update page.tsx

--- src/app/(main)/dashboard/motionxxxx1/app/page.tsx
+++ src/app/(main)/dashboard/motionxxxx1/app/page.tsx
@@ -1,35 +1,34 @@
+Open in Cursor
+Source
+Source code
+Close
 "use client"
-import { Transition } from "motion/react"
-import * as motion from "motion/react-client"
 
-const transition: Transition = {
-    duration: 4,
-    repeat: Infinity,
-    repeatType: "reverse",
-    ease: "easeInOut",
-}
+import { useAnimationFrame } from "motion/react"
+import { useRef } from "react"
 
-export default function MotionPath() {
+export default function UseAnimationFrame() {
+    const ref = useRef<HTMLDivElement>(null)
+
+    useAnimationFrame((t) => {
+        if (!ref.current) return
+
+        const rotate = Math.sin(t / 10000) * 200
+        const y = (1 + Math.sin(t / 1000)) * -50
+        ref.current.style.transform = `translateY(${y}px) rotateX(${rotate}deg) rotateY(${rotate}deg)`
+    })
+
     return (
-        <div style={{ position: "relative" }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="451" height="437">
-                <motion.path
-                    d="M 239 17 C 142 17 48.5 103 48.5 213.5 C 48.5 324 126 408 244 408 C 362 408 412 319 412 213.5 C 412 108 334 68.5 244 68.5 C 154 68.5 102.68 135.079 99 213.5 C 95.32 291.921 157 350 231 345.5 C 305 341 357.5 290 357.5 219.5 C 357.5 149 314 121 244 121 C 174 121 151.5 167 151.5 213.5 C 151.5 260 176 286.5 224.5 286.5 C 273 286.5 296.5 253 296.5 218.5 C 296.5 184 270 177 244 177 C 218 177 197 198 197 218.5 C 197 239 206 250.5 225.5 250.5 C 245 250.5 253 242 253 218.5"
-                    fill="transparent"
-                    strokeWidth="12"
-                    stroke="var(--hue-6-transparent)"
-                    strokeLinecap="round"
-                    initial={{ pathLength: 0 }}
-                    animate={{ pathLength: 1 }}
-                    transition={transition}
-                />
-            </svg>
-            <motion.div
-                style={box}
-                initial={{ offsetDistance: "0%", scale: 2.5 }}
-                animate={{ offsetDistance: "100%", scale: 1 }}
-                transition={transition}
-            />
+        <div className="container">
+            <div className="cube" ref={ref}>
+                <div className="side front" />
+                <div className="side left" />
+                <div className="side right" />
+                <div className="side top" />
+                <div className="side bottom" />
+                <div className="side back" />
+            </div>
+            <StyleSheet />
         </div>
     )
 }
@@ -37,14 +36,78 @@ export default function MotionPath() {
 /**
  * ==============   Styles   ================
  */
+function StyleSheet() {
+    return (
+        <style>{`
+            
+        .container {
+            perspective: 800px;
+            width: 200px;
+            height: 200px;
+        }
 
-const box: React.CSSProperties = {
-    width: 50,
-    height: 50,
-    backgroundColor: "#8df0cc",
-    borderRadius: 10,
-    position: "absolute",
-    top: 0,
-    left: 0,
-    offsetPath: `path("M 239 17 C 142 17 48.5 103 48.5 213.5 C 48.5 324 126 408 244 408 C 362 408 412 319 412 213.5 C 412 108 334 68.5 244 68.5 C 154 68.5 102.68 135.079 99 213.5 C 95.32 291.921 157 350 231 345.5 C 305 341 357.5 290 357.5 219.5 C 357.5 149 314 121 244 121 C 174 121 151.5 167 151.5 213.5 C 151.5 260 176 286.5 224.5 286.5 C 273 286.5 296.5 253 296.5 218.5 C 296.5 184 270 177 244 177 C 218 177 197 198 197 218.5 C 197 239 206 250.5 225.5 250.5 C 245 250.5 253 242 253 218.5")`,
+        .cube {
+            width: 200px;
+            height: 200px;
+            position: relative;
+            transform-style: preserve-3d;
+        }
+
+        .side {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-color: red;
+            opacity: 0.6;
+        }
+
+        .front {
+            transform: rotateY(0deg) translateZ(100px);
+            background-color: var(--hue-1-transparent);
+        }
+        .right {
+            transform: rotateY(90deg) translateZ(100px);
+            background-color: var(--hue-2-transparent);
+        }
+        .back {
+            transform: rotateY(180deg) translateZ(100px);
+            background-color: var(--hue-3-transparent);
+        }
+        .left {
+            transform: rotateY(-90deg) translateZ(100px);
+            background-color: var(--hue-4-transparent);
+        }
+        .top {
+            transform: rotateX(90deg) translateZ(100px);
+            background-color: var(--hue-5-transparent);
+        }
+        .bottom {
+            transform: rotateX(-90deg) translateZ(100px);
+            background-color: var(--hue-6-transparent);
+        }
+
+    `}</style>
+    )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
